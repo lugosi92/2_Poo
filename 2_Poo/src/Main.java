716f460e0a47f6f9cc90aabc937339edb54d5aac
khaled___beno <khaled___beno@localhost>
2023-12-26 19:42:07 +0100

    Arrays con For

---
+++ 2_Poo/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Inicio del programa");
+		
+		//Se crea 2 objetos
+		
+		Persona Hombre = new Persona();
+		Persona mujer = new Persona();
+	
+		
+		
+		
+	}
+	
+}
+
+
+//Que ees un objeto
+/*Una abstraccion de la realidad*/
+
+
+
+//Que es una clase
+/* L a abstraccion de ese objeto en un lenguaje de programacion*/
+
---
+++ 2_Poo/src/Persona.java
@@ -0,0 +1,32 @@
+
+public class Persona {
+
+	//Atributos o caractersiticas
+	
+	String nombre;
+	String apellido;
+	String apodo;
+	int edad;
+	int altura;
+	int peso;
+	
+	
+	
+	//metodos o funciones
+	public void comer() {
+		
+	}
+	
+	
+	public void correr() {
+		
+	}
+	
+	public void caminar() {
+		
+	}
+	
+	public void dormir() {
+		
+	}
+}
